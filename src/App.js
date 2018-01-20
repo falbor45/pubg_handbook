464c6858ec19d4ca8a25bcc3649499d5fab8a44f
Falborski Artur <falborski.artur@gmail.com>
2018-01-20 16:06:16 +0100

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,58 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import "./App.css"
+import {
+  Container,
+  Collapse,
+  Row,
+  Col,
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  Nav,
+  NavItem,
+  NavLink
+} from 'reactstrap';
 
-class App extends Component {
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Container>
+          <Row>
+            <Col xs="12" sm="12" md="12" lg={{size: 10, offset: 1}} xl={{size: 10, offset: 1}}>
+              <Navbar color="faded" dark expand="md">
+                <NavbarBrand href="/">PUBG Handbook</NavbarBrand>
+                <NavbarToggler onClick={this.toggle}/>
+                <Collapse isOpen={this.state.isOpen} navbar>
+                <Nav className="ml-auto" navbar>
+                  <NavItem>
+                    <NavLink href="#">Weapons</NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink href="#">Scopes</NavLink>
+                  </NavItem>
+                </Nav>
+                </Collapse>
+              </Navbar>
+            </Col>
+          </Row>
+        </Container>
       </div>
     );
   }
 }
-
-export default App;
