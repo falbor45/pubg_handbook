be0f32bb312eed5c08635e845bd0fcd6542560e9
Falborski Artur <falborski.artur@gmail.com>
2018-01-28 12:24:41 +0100

    Moved SearchPlayer component away from App component

--- src/App.js
+++ src/App.js
@@ -11,10 +11,8 @@ import {
   Nav,
   NavItem,
   NavLink,
-  ButtonGroup,
-  Button,
-  Input
 } from 'reactstrap';
+import SearchPlayer from './SearchPlayer'
 
 export default class App extends React.Component {
   constructor(props) {
@@ -22,43 +20,21 @@ export default class App extends React.Component {
 
     this.toggle = this.toggle.bind(this);
     this.state = {
+      selectedSection: null,
       isOpen: false,
-      isSelectedQueue: [false, false, false, false, false, false],
-      isSelectedServer: [false, false, false, false, false, false, false],
-      selectedQueue: null,
-      selectedServer: null
     };
+  }
+
+  changeSection = section => {
+    this.setState({
+      selectedSection: section
+    })
   }
 
   toggle() {
     this.setState({
       isOpen: !this.state.isOpen
     });
-  }
-
-  setQueue(which, type, selected) {
-    if (type === 'server') {
-      let changedSelection = [false, false, false, false, false, false, false];
-      changedSelection[which] = true;
-      this.setState({
-        isSelectedServer: changedSelection,
-        selectedServer: selected
-      })
-    }
-    if (type === 'queue') {
-      let changedSelection = [false, false, false, false, false, false];
-      changedSelection[which] = true;
-      this.setState({
-        isSelectedQueue: changedSelection,
-        selectedQueue: selected
-      })
-    }
-  }
-
-  searchPlayer = () => {
-    if (this.state.selectedQueue === null || this.state.selectedServer === null) {
-      return null;
-    }
   }
 
   render() {
@@ -73,63 +49,22 @@ export default class App extends React.Component {
                 <Collapse isOpen={this.state.isOpen} navbar>
                 <Nav className="ml-auto" navbar>
                   <NavItem>
-                    <NavLink href="#">Weapons</NavLink>
+                    <NavLink onClick={() => this.changeSection('searchPlayer')}>Search</NavLink>
                   </NavItem>
                   <NavItem>
-                    <NavLink href="#">Scopes</NavLink>
+                    <NavLink onClick={() => this.changeSection('weapons')}>Weapons</NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink onClick={() => this.changeSection('scopes')}>Scopes</NavLink>
                   </NavItem>
                 </Nav>
                 </Collapse>
               </Navbar>
             </Col>
           </Row>
-          <Row>
-            <Col className="Search-selection" xs="12" sm="12" md="12" lg={{size: 10, offset: 1}}
-                 xl={{size: 10, offset: 1}}>
-              <ButtonGroup>
-                <Button active={this.state.isSelectedServer[0]}
-                        onClick={() => this.setQueue(0, 'server', 'na')}>NA</Button>
-                <Button active={this.state.isSelectedServer[1]}
-                        onClick={() => this.setQueue(1, 'server', 'eu')}>EU</Button>
-                <Button active={this.state.isSelectedServer[2]}
-                        onClick={() => this.setQueue(2, 'server', 'as')}>AS</Button>
-                <Button active={this.state.isSelectedServer[3]}
-                        onClick={() => this.setQueue(3, 'server', 'oc')}>OC</Button>
-                <Button active={this.state.isSelectedServer[4]}
-                        onClick={() => this.setQueue(4, 'server', 'sa')}>SA</Button>
-                <Button active={this.state.isSelectedServer[5]}
-                        onClick={() => this.setQueue(5, 'server', 'sea')}>SEA</Button>
-                <Button active={this.state.isSelectedServer[6]}
-                        onClick={() => this.setQueue(6, 'server', 'krjp')}>KRJP</Button>
-              </ButtonGroup>
-            </Col>
-          </Row>
-          <Row>
-            <Col className="Search-selection" xs="12" sm="12" md="12" lg={{size: 10, offset: 1}}
-                 xl={{size: 10, offset: 1}}>
-              <ButtonGroup>
-                <Button active={this.state.isSelectedQueue[0]} onClick={() => this.setQueue(0, 'queue', 'solo')}>Solo
-                  TPP</Button>
-                <Button active={this.state.isSelectedQueue[1]} onClick={() => this.setQueue(1, 'queue', 'solo-fpp')}>Solo
-                  FPP</Button>
-                <Button active={this.state.isSelectedQueue[2]} onClick={() => this.setQueue(2, 'queue', 'duo')}>Duo
-                  TPP</Button>
-                <Button active={this.state.isSelectedQueue[3]} onClick={() => this.setQueue(3, 'queue', 'duo-fpp')}>Duo
-                  FPP</Button>
-                <Button active={this.state.isSelectedQueue[4]} onClick={() => this.setQueue(4, 'queue', 'squad')}>Squad
-                  TPP</Button>
-                <Button active={this.state.isSelectedQueue[5]} onClick={() => this.setQueue(5, 'queue', 'squad-fpp')}>Squad
-                  FPP</Button>
-              </ButtonGroup>
-            </Col>
-          </Row>
-          <Row>
-            <Col className="Search-selection" xs="12" sm="12" md="12" lg={{size: 10, offset: 1}}
-                 xl={{size: 6, offset: 3}}>
-              <Input placeholder="Type in name of the player you search for "/>
-              <Button onClick={this.searchPlayer}>Search</Button>
-            </Col>
-          </Row>
+          {
+            this.state.selectedSection === 'searchPlayer' ? <SearchPlayer/> : null
+          }
         </Container>
       </div>
     );
